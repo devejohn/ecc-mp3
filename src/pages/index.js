dc3b564f4e1638fe19dce91642488bf727958d2d
devejohn <devejohn@icloud.com>
2021-11-26 17:41:47 -0500

    update cred

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,46 @@
-import React from "react";
-import Meta from "./../components/Meta";
-import HeroSection from "./../components/HeroSection";
-import FeaturesSection from "./../components/FeaturesSection";
-import NewsletterSection from "./../components/NewsletterSection";
+import React from 'react'
+import Meta from './../components/Meta'
+import HeroSection from './../components/HeroSection'
+import FeaturesSection from './../components/FeaturesSection'
+import NewsletterSection from './../components/NewsletterSection'
 
 function IndexPage(props) {
-  return (
-    <>
-      <Meta />
-      <HeroSection
-        bgColor="primary"
-        size="large"
-        bgImage="https://images.squarespace-cdn.com/content/v1/5de67da1631c6c6aca04d6d1/1609606031611-4HAH38DNKIQOBJKHTXFP/23521664293_b86c26ed5d_o.jpg"
-        bgImageOpacity={0.7}
-        title="Come, let us sing joyfully to the LORD"
-        subtitle="You've heard it. You love it. Now, you can listen to it whenever you want. Download the Gospel music from the talented singers and musicians from Epiphany Catholic Church."
-        buttonText="Get them today"
-        buttonColor="secondary"
-        buttonPath="https://epiphanycatholicchurch.weshareonline.org/"
-      />
-      <FeaturesSection
-        bgColor="secondary"
-        size="normal"
-        bgImage=""
-        bgImageOpacity={1}
-        title="Music that moves you"
-        subtitle="All your favorites"
-      />
-      <NewsletterSection
-        bgColor="default"
-        size="medium"
-        bgImage=""
-        bgImageOpacity={1}
-        title="Epiphany Happenings"
-        subtitle="Celebrate the Eucharist. Living the Gospel, Loving God and Loving Neighbor"
-        buttonText="Subscribe"
-        buttonColor="primary"
-        inputPlaceholder="Enter your email"
-        subscribedMessage="You are now subscribed!"
-      />
-    </>
-  );
+	return (
+		<>
+			<Meta />
+			<HeroSection
+				bgColor='primary'
+				size='large'
+				bgImage='https://images.squarespace-cdn.com/content/v1/5de67da1631c6c6aca04d6d1/1609606031611-4HAH38DNKIQOBJKHTXFP/23521664293_b86c26ed5d_o.jpg'
+				bgImageOpacity={0.7}
+				title='Come, let us sing joyfully to the LORD'
+				subtitle="You've heard it. You love it. Now, you can listen to it whenever you want. Download the Gospel music from the talented singers and musicians from Epiphany Catholic Church."
+				buttonText='Get them today'
+				buttonColor='secondary'
+				buttonPath='https://epiphanycatholicchurch.weshareonline.org/'
+			/>
+			<FeaturesSection
+				bgColor='secondary'
+				size='normal'
+				bgImage=''
+				bgImageOpacity={1}
+				title='Music that moves you'
+				subtitle='All your favorites'
+			/>
+			<NewsletterSection
+				bgColor='default'
+				size='medium'
+				bgImage=''
+				bgImageOpacity={1}
+				title='Epiphany Happenings'
+				subtitle='Celebrate the Eucharist. Living the Gospel, Loving God and Loving Neighbor'
+				buttonText='Subscribe'
+				buttonColor='primary'
+				inputPlaceholder='Enter your email'
+				subscribedMessage='You are now subscribed!'
+			/>
+		</>
+	)
 }
 
-export default IndexPage;
+export default IndexPage
